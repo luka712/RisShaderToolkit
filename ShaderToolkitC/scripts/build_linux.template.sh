eabf7bb39ce408b88e5846c37b652d5b10e91730
lerkapic <erkapic.luka.os@gmail.com>
2026-02-17 22:07:05 +0100

    readme

--- ShaderToolkitC/scripts/build_linux.template.sh
+++ ShaderToolkitC/scripts/build_linux.template.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+set -euo pipefail
 
-export VCPKG_ROOT=/path/tp/vcpkg
-export PATH=$VCPKG_ROOT:$PATH
+export VCPKG_ROOT=#ADD VCKPG PATH
+export PATH="$VCPKG_ROOT:$PATH"
+
+BUILD_TYPE="${1:-Debug}"
+BUILD_DIR="cmake-build-${BUILD_TYPE,,}"  # debug/release lowercase
 
 # Build
 cmake \
   -G Ninja \
-  -B cmake-build-debug-linux-xcode \
+  -B "$BUILD_DIR" \
   -S . \
-  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  -DCMAKE_TOOLCHAIN_FILE=//path/tp/vcpkg/scripts/buildsystems/vcpkg.cmake
+  -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
 
-cmake --build cmake-build-debug
+cmake --build "$BUILD_DIR"
